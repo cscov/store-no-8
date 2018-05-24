d192fabfe0be61b9d92afa61aa0dd08bfe774b3f
cscov <carolyn.l.scoville@gmail.com>
2018-05-24 14:56:08 -0700

    added order summary column to orders

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 require 'rspec'
-require 'routes'
 
 RSpec.describe Api::SessionsController, type: :controller do
-  it "displays the user's orders after successful login"
-  it "displays the user's orders after successful account creation"
+  it "displays the user's orders after successful login" #do
+  # end
+  # it "displays the user's orders after successful account creation" do
+  #   user = User.create!(email_address: "test3@gmail.com", password: "123456", first_name: "Carolyn")
+  #   should route('get', "api/users/#{user.id}/orders").to(action: 'index')
+  # end
   it 'flashes errors on unsuccessful login'
   it 'flashes errors on unsuccessful account creation'
   it 'displays the login screen upon succesful log out'
