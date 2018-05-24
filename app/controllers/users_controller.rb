e95f1b5bbc2f2109404e064f8a7f1eca391ad601
cscov <carolyn.l.scoville@gmail.com>
2018-05-24 09:39:50 -0700

    Added users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
   end
 
   def new
+    @user = User.new
+  end
+
+  def user_params(first_name, mail_address, password)
+    params.require(:user).permit(first_name, mail_address, password)
   end
 end
