b93b25db44d83e593c4691badce0c2259b5b2fe0
cscov <carolyn.l.scoville@gmail.com>
2018-05-25 15:27:12 -0700

    Added item controller and spec

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   def index
-
+    Index.all
   end
 
   def show
-
+    @item = Item.find(params[:id])
   end
 end
--- spec/controllers/api/items_controller_spec.rb
+++ spec/controllers/api/items_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
+require 'rspec'
 
 RSpec.describe ItemsController, type: :controller do
+  describe "GET #index" do
+    it "displays all available items" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
 
+  describe "GET #show" do
+    it "displays the selected item" do
+      @item = Item.create(name: "broccoli", price: 2.99)
+
+      get :show, params: { id: @item.id }
+      expect(response).to render_template(:show)
+
+    end
+  end
 end
